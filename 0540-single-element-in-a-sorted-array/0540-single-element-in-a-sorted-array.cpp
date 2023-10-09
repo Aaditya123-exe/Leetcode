3b1832b4327fd63f1e5732e40c523dc20ad75715
Aaditya Patil <123950344+Aaditya123-exe@users.noreply.github.com>
2023-10-09 10:33:55 +0530

    Time: 18 ms (72.79%), Space: 22.6 MB (35.81%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -24,23 +24,28 @@ public:
             int mid = (low + high) / 2;
             if (mid & 1)
             {
+                // check if other occ of element is at its prev even then its following pattern nd hence single number lies after it
                 if (nums[mid - 1] == nums[mid])
                 {
                     low = mid + 1;
                 }
+                // if not following then single number lies before it
                 else
-                    high = mid;
+                    high = mid - 1;
             }
             else
             {
+                // check if next occ of element lies to right odd if lies then it follows pattern nd single is lies aftet it
                 if (mid + 1 < n && nums[mid] == nums[mid + 1])
                 {
                     low = mid + 1;
                 }
+                // check if its other occ is at its left odd then single number lies before it
                 else if (mid - 1 >= 0 && nums[mid] == nums[mid - 1])
                 {
                     high = mid - 1;
                 }
+                // if not found on left or right then curr is single ele
                 else
                     return nums[mid];
             }
