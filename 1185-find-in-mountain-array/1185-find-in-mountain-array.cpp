8520efcec774bc920f064aac5151e74cee424165
Aaditya Patil <123950344+Aaditya123-exe@users.noreply.github.com>
2023-10-12 09:04:14 +0530

    Time: 3 ms (78.78%), Space: 7.3 MB (43.62%) - LeetHub

---
+++ 1185-find-in-mountain-array/1185-find-in-mountain-array.cpp
@@ -0,0 +1,110 @@
+// /**
+//  * // This is the MountainArray's API interface.
+//  * // You should not implement it, or speculate about its implementation
+//  * class MountainArray {
+//  *   public:
+//  *     int get(int index);
+//  *     int length();
+//  * };
+//  */
+
+// class Solution {
+// public:
+//     int findInMountainArray(int target, MountainArray &arr) {
+//         int low=0,high=arr.length()-1;
+//         int peak=0;
+//         while(low<=high)
+//         {
+//             int mid=low+(high-low)/2;
+//             int fval=arr.get(mid-1);
+//             int mval=arr.get(mid);
+//             int lval=arr.get(mid+1);
+//             if(mval>lval&&mval>fval)
+//             {
+//                 peak=mid;
+//                 break;
+//             }
+//             else if(mval>lval)
+//                 high=mid-1;
+//             else
+//                 low=mid+1;
+//         }
+//         low=0,high=peak;
+//         while(low<=high)
+//         {
+//             int mid=low+(high-low)/2;
+//             int mval=arr.get(mid);
+//             if(mval==target)
+//                 return mid;
+//             else if(mval<target)
+//                 low=mid+1;
+//             else
+//                 high=mid-1;
+//         }
+//         low=peak+1;
+//         high=arr.length()-1;
+//         while(low<=high)
+//         {
+//             int mid=low+(high-low)/2;
+//             int mval=arr.get(mid);
+//             if(mval==target)
+//                 return mid;
+//             else if(mval>target)
+//                 low=mid+1;
+//             else
+//                 high=mid-1;
+//         }
+//         return -1;
+//     }
+// };
+class Solution {
+public:
+    int findInMountainArray(int target, MountainArray &mountainArr) 
+    {
+        int start = 0;
+        int end = mountainArr.length()-1;
+        int mid = start + (end - start)/2;
+        while(start < end)
+        {
+            if(mountainArr.get(mid) < mountainArr.get(mid+1))
+                start = mid +1;
+            else 
+                end = mid;
+            mid = start + (end - start)/2;
+        }
+        int peak = mid;
+        int left = -1, right = -1;
+
+        start = 0;
+        end = peak;
+        mid = start + (end - start)/2;
+        while(start <= end)
+        {
+            int middle = mountainArr.get(mid);
+            if(middle == target)
+                return mid;
+            if(middle < target )
+                start = mid + 1;
+            else 
+                end = mid - 1;
+            mid = start + (end - start)/2;
+        }
+        
+        start = peak;
+        end = mountainArr.length()-1;
+        mid = start + (end - start)/2;
+        while(start <= end)
+        {
+            int middle = mountainArr.get(mid);
+            if(middle == target)
+                return mid;
+            if(middle > target )
+                start = mid + 1;
+            else 
+                end = mid - 1;
+            mid = start + (end - start)/2;
+        }
+
+      return -1;
+    }
+};
