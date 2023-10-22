64150a7d7e78db3dbbaa678f3217a1b95d374aef
Aaditya Patil <123950344+Aaditya123-exe@users.noreply.github.com>
2023-10-22 11:41:07 +0530

    Time: 182 ms (61.54%), Space: 126.4 MB (84.62%) - LeetHub

--- 3166-minimum-number-of-groups-to-create-a-valid-assignment/3166-minimum-number-of-groups-to-create-a-valid-assignment.cpp
+++ 3166-minimum-number-of-groups-to-create-a-valid-assignment/3166-minimum-number-of-groups-to-create-a-valid-assignment.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
-    bool isPossible(unordered_map<int, int>& mp,int x) {
-        for (auto it : mp) {
-            int rem = it.second % x;
-            int q = it.second / x;
-            if (rem < x - 1) {
-                int req = (x - 1) - rem;
-                if (q >= req) rem = x - 1;
-            }
-            if (0 < rem && rem < x - 1) return false;
+    int group(unordered_map<int,int>&m,int g){
+        int ans = 0;
+        for(auto i:m){
+           int a = i.second;
+           if(a<g){
+               return 1e9;
+           }
+           int s = 0;
+           s+=a/(g+1);
+           a = a%(g+1);
+           if(a!=0 && g-a<=s){
+               s++;
+           }
+           else if(a!=0){
+               return 1e9;
+           }
+           ans+=s;
         }
-        return true;
+        return ans;
     }
-    
     int minGroupsForValidAssignment(vector<int>& nums) {
-        int n = nums.size();
-        int mini = n;
-        unordered_map<int, int> mp;
-        for (auto num:nums) mp[num]++;
-        for (auto it:mp) mini = min(mini, it.second);
-        for (int i = mini + 1; i > 0; i--) {
-            if (isPossible(mp,i)) {
-                int out = 0;
-                for (auto it : mp) {
-                    int rem = it.second % i;
-                    out += it.second / i;
-                    if (rem > 0) out++;
-                }
-                return out;
-            }
+        if(nums.size()==1)return 1;
+        int ans=INT_MAX;
+        unordered_map<int,int>m;
+        for(auto i:nums){
+            m[i]++;
         }
-        return -1;
+        for(int i = 1;i<nums.size();i++){
+            ans = min(ans,group(m,i));
+        }
+        return ans;
+        
     }
 };
