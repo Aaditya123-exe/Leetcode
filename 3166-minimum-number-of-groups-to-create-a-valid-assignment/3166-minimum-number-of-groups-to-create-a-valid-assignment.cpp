a6462976d7e2241706059262b2b46ccdb8885a20
Aaditya Patil <123950344+Aaditya123-exe@users.noreply.github.com>
2023-10-22 11:47:12 +0530

    Time: 171 ms (84.62%), Space: 126.7 MB (61.54%) - LeetHub

--- 3166-minimum-number-of-groups-to-create-a-valid-assignment/3166-minimum-number-of-groups-to-create-a-valid-assignment.cpp
+++ 3166-minimum-number-of-groups-to-create-a-valid-assignment/3166-minimum-number-of-groups-to-create-a-valid-assignment.cpp
@@ -1,36 +1,51 @@
-class Solution {
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
 public:
-    int group(unordered_map<int,int>&m,int g){
+    int group(unordered_map<int, int> &m, int g)
+    {
         int ans = 0;
-        for(auto i:m){
-           int a = i.second;
-           if(a<g){
-               return 1e9;
-           }
-           int s = 0;
-           s+=a/(g+1);
-           a = a%(g+1);
-           if(a!=0 && g-a<=s){
-               s++;
-           }
-           else if(a!=0){
-               return 1e9;
-           }
-           ans+=s;
+        for (auto i : m)
+        {
+            int a = i.second;
+            if (a < g)
+            {
+                return 1e9;
+            }
+            int s = 0;
+            s += a / (g + 1);
+            a = a % (g + 1);
+            if (a != 0 && g - a <= s)
+            {
+                s++;
+            }
+            else if (a != 0)
+            {
+                return 1e9;
+            }
+            ans += s;
         }
         return ans;
     }
-    int minGroupsForValidAssignment(vector<int>& nums) {
-        if(nums.size()==1)return 1;
-        int ans=INT_MAX;
-        unordered_map<int,int>m;
-        for(auto i:nums){
+    int minGroupsForValidAssignment(vector<int> &nums)
+    {
+        if (nums.size() == 1)
+            return 1;
+        int ans = INT_MAX;
+        unordered_map<int, int> m;
+        for (auto i : nums)
+        {
             m[i]++;
         }
-        for(int i = 1;i<nums.size();i++){
-            ans = min(ans,group(m,i));
+        int mn = INT_MAX;
+        for (auto i : m)
+            mn = min(mn, i.second);
+        for (int i = 1; i <= mn; i++)
+        {
+            ans = min(ans, group(m, i));
         }
         return ans;
-        
     }
 };
